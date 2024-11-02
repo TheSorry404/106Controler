ce823ff5b9809986aa4482a69b4b83ec1f70a313
Syan Wang <thesorry404@outlook.com>
2024-11-02 20:00:04 +0800

    base

---
+++ 南区活动中心会议室控制台/南区活动中心会议室控制台/____________App.swift
@@ -0,0 +1,18 @@
+//
+//  ____________App.swift
+//  南区活动中心会议室控制台
+//
+//  Created by 牢大 on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//            _06ActivityUI()
+        }
+    }
+}
